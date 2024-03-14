d167c373a3e307c7ae49f3dc77cf986055fb4824
pwtansg <pwtan.edu@gmail.com>
2024-03-14 22:53:15 +0800

    initial basic QR barcode scanner using html5-qrcode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { Html5QrcodeScanner } from 'html5-qrcode';
+import { useState, useEffect, useRef } from 'react'
 
 function App() {
+
+  const mount = useRef(true);
+  const [scanResult, setScanResult] = useState(null);
+  const [validUrl, setValidUrl] = useState(false);
+
+  useEffect(() => {
+
+    function validateURL(url) {
+      if (/^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g.test(url)) {
+        console.log('Valid');
+        setValidUrl(true)
+      } else {
+        console.log('Not Valid');
+      }
+    }
+
+    function onScanSuccess(decodedText, decodedResult) {
+      // console.log(`Code matched = ${decodedText}`, decodedResult);
+      validateURL(decodedText)
+      setScanResult(decodedText)
+    }
+
+    function onScanFailure(error) {
+      console.warn(`Code scan error = ${error}`);
+    }
+
+    const scanner = new Html5QrcodeScanner('reader', {
+      qrbox: {
+        width: 250,
+        height: 250
+      },
+      fps: 5
+    });
+
+
+    if (mount.current) {
+      scanner.render(onScanSuccess, onScanFailure);
+    }
+
+    return () => {
+      mount.current = false;
+    }
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Scanner</h1>
+      <div className='main'>
+        <div id="reader"></div>
+      </div>
+      {scanResult
+        ? validUrl
+          ? <div><a href={scanResult}>{scanResult}</a></div>
+          : <div>{scanResult}</div>
+        : <div></div>
+      }
+      {/* <div id="reader1"></div> */}
+
+    </div >
   );
 }
 
